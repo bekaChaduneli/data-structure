f4561a968ee2577bc72967d76fd5835ceaa6f0a5
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-08 23:05:11 +0400

    linked list implement processing

---
+++ linkedList.js
@@ -0,0 +1,36 @@
+const { json } = require("express");
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+  getSize() {
+    return this.size;
+  }
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+}
+
+const list = new LinkedList();
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
