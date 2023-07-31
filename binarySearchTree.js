f67e9577b169897d87bb2d21c8139ae7d1d616dd
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-31 17:56:01 +0400

    implement delete function in binary tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -107,6 +107,33 @@ class BinarySearchTree {
             return this.max(root.right);
         }
     }
+
+    delete(value) {
+        this.root = this.deleteNode(this.root, value);
+    }
+
+    deleteNode(root, value) {
+        if (root === null) {
+            return root;
+        }
+        if (value < root.value) {
+            root.left = this.deleteNode(root.left, value);
+        } else if (value > root.value) {
+            root.right = this.deleteNode(root.right, value);
+        } else {
+            if (!root.left && !root.right) {
+                return null;
+            }
+            if (!root.left) {
+                return root.right;
+            } else if (!root.right) {
+                return root.left;
+            }
+            root.value = this.min(root.right);
+            root.right = this.deleteNode(root.right, root.value);
+        }
+        return root;
+    }
 }
 
 const bst = new BinarySearchTree();
@@ -118,10 +145,7 @@ bst.insert(15);
 bst.insert(3);
 bst.insert(7);
 
-console.log(bst.search(bst.root, 10));
-console.log(bst.search(bst.root, 5));
-console.log(bst.search(bst.root, 15));
-console.log(bst.search(bst.root, 20));
+bst.levelOrder();
 
-console.log(bst.min(bst.root));
-console.log(bst.max(bst.root));
+bst.delete(15);
+bst.levelOrder();
