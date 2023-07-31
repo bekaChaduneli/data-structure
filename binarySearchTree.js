9304b5ab15078521e915befc066e15fa7a58cab9
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-31 17:17:06 +0400

    implement depth search in binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -53,6 +53,29 @@ class BinarySearchTree {
             }
         }
     }
+
+    preOrder(root) {
+        if (root) {
+            console.log(root.value);
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+        }
+    }
+    inOrder(root) {
+        if (root) {
+            this.inOrder(root.left);
+            console.log(root.value);
+            this.inOrder(root.right);
+        }
+    }
+
+    postOrder(root) {
+        if (root) {
+            this.postOrder(root.left);
+            this.postOrder(root.right);
+            console.log(root.value);
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
@@ -61,8 +84,11 @@ console.log("Tree is empty?", bst.isEmpty());
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+bst.insert(3);
+bst.insert(7);
 
 console.log(bst.search(bst.root, 10));
 console.log(bst.search(bst.root, 5));
 console.log(bst.search(bst.root, 15));
 console.log(bst.search(bst.root, 20));
+bst.postOrder(bst.root);
