0250c7f178661a7e16a10a39ffee0fca8a5485b9
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-31 18:57:19 +0400

    implement display and hasEdge function in graph

--- graph.js
+++ graph.js
@@ -31,6 +31,19 @@ class Graph {
         this.adjacencyList[vertex1].add(vertex2);
         this.adjacencyList[vertex2].add(vertex1);
     }
+
+    hasEdge(vertex1, vertex2) {
+        return (
+            this.adjacencyList[vertex1].has(vertex2) &&
+            this.adjacencyList[vertex2].has(vertex1)
+        );
+    }
+
+    display() {
+        for (let vertex in this.adjacencyList) {
+            console.log(vertex + " -> " + [...this.adjacencyList[vertex]]);
+        }
+    }
 }
 
 const graph = new Graph();
@@ -40,3 +53,6 @@ graph.addVertex("C");
 
 graph.addEdge("A", "B");
 graph.addEdge("B", "C");
+
+graph.display();
+console.log(graph.hasEdge("A", "B"));
